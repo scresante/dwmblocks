9f0f861e10a2c606eab8f461b19ec077c701a660
shawn cresante <shawn@cresante.com>
2020-07-12 16:28:21 -0400

    no weather only important things

--- config.h
+++ config.h
@@ -4,18 +4,18 @@ static const Block blocks[] = {
 	//{"", "cat /tmp/recordingicon 2>/dev/null",	0,	9},
 	/* {"",	"music",	0,	11},*/
 	//{"",	"pacpackages",	0,	8},
-	{"",	"news",		0,	6},
+	//{"",	"news",		0,	6},
 	/* {"",	"crypto",	0,	13}, */
 	/* {"",	"price bat \"Basic Attention Token\" 🦁",	0,	20}, */
 	/* {"",	"price btc Bitcoin 💰",				0,	21}, */
 	/* {"",	"price lbc \"LBRY Token\" 📚",			0,	22}, */
-	{"",	"torrent",	20,	7},
+	//{"",	"torrent",	20,	7},
 	/* {"",	"memory",	10,	14}, */
 	/* {"",	"cpu",		10,	18}, */
 	/* {"",	"moonphase",	18000,	17}, */
-	{"",	"weather",	18000,	5},
-	{"",	"mailbox",	180,	12},
-	/* {"",	"nettraf",	1,	16}, */
+	//{"",	"weather",	18000,	5},
+	//{"",	"mailbox",	180,	12}
+    {"",	"nettraf",	1,	16},
 	{"",	"volume",	0,	10},
 	{"",	"battery",	5,	3},
 	{"",	"clock",	60,	1},
